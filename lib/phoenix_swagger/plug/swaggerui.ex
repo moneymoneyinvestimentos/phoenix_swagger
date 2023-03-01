736c2cb8f7373e417d2d886993c4561af1158128
vitor melo <vitor.melo@chat2desk.com.br>
2023-03-01 09:56:47 -0300

    fix: configObject

--- lib/phoenix_swagger/plug/swaggerui.ex
+++ lib/phoenix_swagger/plug/swaggerui.ex
@@ -111,7 +111,6 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
     const configObject = {
       url: swagger_url.href,
       dom_id: '#swagger-ui',
-      validatorUrl : null,
       deepLinking: true,
       tagsSorter: (a, b) => {
         return a.localeCompare(b);
